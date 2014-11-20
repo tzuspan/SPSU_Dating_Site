adc02a9a31368e825dfbd9252747ab5547715ff5
hgm959 <cfoster6867@gmail.com>
2014-11-19 20:29:54 -0500

    Handled the login

--- Code/check_login.php
+++ Code/check_login.php
@@ -1,25 +1,31 @@
 <?php
 session_start();
+
+include 'dbconnection.php';
+
+/*
 $host="localhost"; // Host name
 $username="root"; // Mysql username
 $password=""; // Mysql password
 $db_name="swe3613_db05p2"; // Database name
-$tbl_name="tbl_users"; // Table name
 
 $conn = mysqli_connect("$host", "$username", "$password", "$db_name")or die("cannot connect");
+*/
+
+$tbl_name="tbl_users"; // Table name
 
 // username and password sent from form
-$myusername=$_POST['myusername'];
+$myusername=$_POST["myusername"];
 $mypassword=$_POST['mypassword'];
 
 // To protect MySQL injection (more detail about MySQL injection)
 $myusername = stripslashes($myusername);
 $mypassword = stripslashes($mypassword);
-$myusername = mysql_real_escape_string($myusername);
-$mypassword = mysql_real_escape_string($mypassword);
+$myusername = mysqli_real_escape_string($dbcon, $myusername);
+$mypassword = mysqli_real_escape_string($dbcon, $mypassword);
 
 $sql="SELECT user_id FROM $tbl_name WHERE email_address='$myusername' and password='$mypassword';";
-$result=mysqli_query($conn, $sql);
+$result=mysqli_query($dbcon, $sql);
 $count=@mysqli_num_rows($result);
 
 // If result matched $myusername and $mypassword, table row must be 1 row
@@ -31,6 +37,7 @@ if($count==1){
 	header("location:pairing_page.php");
 }
 else {
-	echo "Wrong Username or Password";
+	echo "Wrong Username or Password<br />";
+	echo 'SQL: '.$sql.'<br />';
 }
 ?>
