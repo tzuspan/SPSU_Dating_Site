6862d4dc6d649e82dba68a555135a3a0e844a4eb
mitchellbyrd <mbyrd11@gmail.com>
2014-11-22 08:41:13 -0500

    index.php, style1.css, and GiveAGeekSomeLove.png
    index.php
    *Added title
    *Added link to stylesheet
    *Added metaname tag because a guy in a video said it was needed for cell
    phones
    *Added image
    *Made changes to neaten up the table
    
    style1.css
    *Added some colors (will likely be changed
    *Added code to to adjust the screen container for mobile phones
    
    GiveAGeekSomeLove.png
    *Added the image

--- Code/index.php
+++ Code/index.php
@@ -1,33 +1,36 @@
  <!DOCTYPE html>
 <html>
+<head>
+<title>SPSU Dating site</title>
+<link rel="stylesheet" type="text/css" href="css/style1.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
 <body>
-
-<table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+<img src="Images/GiveAGeekSomeLove.png" alt="Give A Geek Some Love" id="logo">
+<table id="login_table" width="350" border="1" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
 <tr>
-<form name="form1" method="post" action="check_login.php">
-<td>
-<table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
-<tr>
-<td colspan="3"><strong>Member Login </strong></td>
-</tr>
-<tr>
-<td width="78">Email Address</td>
-<td width="6">:</td>
-<td width="294"><input name="myusername" type="text" id="myusername"></td>
-</tr>
-<tr>
-<td>Password</td>
-<td>:</td>
-<td><input name="mypassword" type="password" id="mypassword"></td>
-</tr>
-<tr>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td><input type="submit" name="Submit" value="Login"></td>
-</tr>
-</table>
-</td>
-</form>
+	<form name="form1" method="post" action="check_login.php">
+	<td>
+		<table id="login_table" width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+			<tr>
+				<td colspan="3" id="top"><strong>Member Login </strong></td>
+			</tr>
+			<tr>
+				<td width="30%" id="login_col_left">Email Address</td>
+				<td width="2%">:</td>
+				<td width="68%"><input name="myusername" type="text" id="myusername" style="width:98%"></td>
+			</tr>
+			<tr>
+				<td id="login_col_left">Password</td>
+				<td>:</td>
+				<td><input name="mypassword" type="password" id="mypassword" style="width:98%"></td>
+			</tr>
+			<tr>
+				<td colspan="3" align="center"><input type="submit" name="Submit" value="Login"></td>
+			</tr>
+		</table>
+	</td>
+	</form>
 </tr>
 </table>
 <a href="create_user_account.php" align="center" ><p style="text-align:center">Create User</p></a>
