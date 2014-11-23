3dd61eda1435798531d1d54d50f712176b5e59b7
hgm959 <cfoster6867@gmail.com>
2014-11-23 13:16:04 -0500

    Adding the send_message.php file to allow users to reply to a specific message when they are communicating.

---
+++ Code/send_message.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>SPSU Dating site</title>
+<link rel="stylesheet" type="text/css" href="css/style1.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php session_start(); ?>
+</head>
+<body>
+<?php include 'navigation_bar.php'; ?>
+<?php echo 'uid : '.$_SESSION["uid"]; ?>
+
+</body>
+</html>
