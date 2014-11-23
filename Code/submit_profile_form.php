237e5dbba971fcda1d9f843e98f3245cef6c990a
tzuspan <tzuspan@gmail.com>
2014-11-23 18:53:39 -0500

    switched out forms

--- Code/submit_profile_form.php
+++ Code/submit_profile_form.php
@@ -7,75 +7,28 @@
 <?php session_start(); ?>
 </head>
 <body>
-<?php
-	include 'dbconnection.php';
-	include 'navigation_bar.php';
-
-	echo 'uid : '.$_SESSION["uid"];
-	// If the user is not logged in yet
-	// inform them they must be, then
-	// provide a link to the login page.
-	if(!isset($_SESSION["uid"]))
-	{
-		echo "You must be logged in to view this page! Please ";
-		echo '<a href="index.php">login</a> to continue';
-	}
-	else
-	{
-		$userId = $_SESSION["uid"];
-
-		$query = "SELECT first_name FROM tbl_users where user_id='$userId'";
-		$firstName = mysqli_query($dbcon, $query);
-		$query = "SELECT last_name FROM tbl_users where user_id='$userId'";
-		$lastName = mysqli_query($dbcon, $query);
-		$query = "SELECT email_address FROM tbl_users where user_id='$userId'";
-		$emailAddress = mysqli_query($dbcon, $query);
-		$query = "SELECT password FROM tbl_users where user_id='$userId'";
-		$password = mysqli_query($dbcon, $query);
-		$query = "SELECT gender FROM tbl_users where user_id='$userId'";
-		$gender = mysqli_query($dbcon, $query);
-		$query = "SELECT age FROM tbl_users where user_id='$userId'";
-		$age = mysqli_query($dbcon, $query);
-		$query = "SELECT female_friend FROM tbl_users where user_id='$userId'";
-		$ff = mysqli_query($dbcon, $query);
-		$query = "SELECT male_friend FROM tbl_users where user_id='$userId'";
-		$mf = mysqli_query($dbcon, $query);
-		$query = "SELECT female_date FROM tbl_users where user_id='$userId'";
-		$fd = mysqli_query($dbcon, $query);
-		$query = "SELECT male_date FROM tbl_users where user_id='$userId'";
-		$md = mysqli_query($dbcon, $query);
-		$query = "SELECT about_me FROM tbl_users where user_id='$userId'";
-		$aboutMe = mysqli_query($dbcon, $query);
-	}
-?>
+<?php include 'navigation_bar.php'; ?>
+<?php echo 'uid : '.$_SESSION["uid"]; ?>
 
 <div>
 	<form action="profile_form.php">
 		First Name:<br>
-		<input required type="text" name="FirstName"	value="<?php echo $firstName;?>"><br><br>
+		<input required type="text" name="FirstName"		value="firstName"><br><br>
 		Last Name:<br>
-		<input required type="text" name="LastName"		value="<?php echo $lastName;?>"><br><br>
+		<input required type="text" name="LastName"		value="lastName"><br><br>
 		Email Address:<br>
-		<input required type="text" name="EmailAddress"	value="<?php echo $emailAddress;?>"><br><br>
+		<input required type="text" name="EmailAddress"	value="emailAddress"><br><br>
 		Password:<br>
-		<input required type="text" name="Password"		value="<?php echo $password;?>"><br><br>
+		<input required type="text" name="Password"		value="password"><br><br>
 
 		Gender<br>
 		<select required name="Gender">
-		<?php
-		if ($gender=='male') {
-			echo "<option value="male">Male</option>";
-			echo "<option value="female">Female</option>";
-		} else {
-			echo "<option value="female">Female</option>";
-			echo "<option value="male">Male</option>";
-		}
-		?>
+			<option value="male">Male</option>
+			<option value="female">Female</option>
 		</select><br><br>
 
 		Age<br>
 		<select name="Age">
-		<option value="<?php echo $age;?>"><?php echo $age;?></option>
 		<?php
 			for ($i=1; $i<=125; $i++)
 			{
@@ -87,23 +40,17 @@
 		</select><br><br>
 
 		Interested in<br>
-		<input id="femaleFriend" name="FemaleFriend" value="femaleFriend" type="checkbox"
-			<?php if ($ff=='1') {echo <"checked="checked">"} ?> >
+		<input id="femaleFriend" name="FemaleFriend" value="femaleFriend" type="checkbox">
 		Befriending Women<br>
-		<input id="maleFriend" name="MaleFriend" value="maleFriend" type="checkbox"
-			<?php if ($mf=='1') {echo <"checked="checked">"} ?> >
+		<input id="maleFriend" name="MaleFriend" value="maleFriend" type="checkbox">
 		Befriending Men<br>
-		<input id="femaleDate" name="FemaleDate" value="femaleDate" type="checkbox"
-			<?php if ($fd=='1') {echo <"checked="checked">"} ?> >
+		<input id="femaleDate" name="FemaleDate" value="femaleDate" type="checkbox">
 		Dating Women<br>
-		<input id="maleDate" name="MaleDate" value="maleDate" type="checkbox"
-			<?php if ($md=='1') {echo <"checked="checked">"} ?> >
+		<input id="maleDate" name="MaleDate" value="maleDate" type="checkbox">
 		Dating Men<br><br>
 
 		About Me<br>
-		<textarea rows="4" cols="50" name="AboutMe" maxlength="5000">
-			<?php echo $aboutMe; ?>
-		</textarea><br><br>
+		<textarea rows="4" cols="50" name="AboutMe" maxlength="5000">about me text</textarea><br><br>
 
 		<input type="submit" value="Submit">
 	</form>
