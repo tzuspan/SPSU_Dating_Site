37abe9d3e27ae7e99c29c7c4f0dd81e751da0c93
tzuspan <tzuspan@gmail.com>
2014-11-23 20:50:48 -0500

    more work

--- Code/submit_profile_form.php
+++ Code/submit_profile_form.php
@@ -5,7 +5,6 @@
 <link rel="stylesheet" type="text/css" href="css/style1.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php
-	session_start();
 	error_reporting(E_ALL);
 ?>
 </head>
