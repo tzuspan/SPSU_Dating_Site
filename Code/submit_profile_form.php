c9a415ec611732a6b97f99dff856e6a6216d7dca
tzuspan <tzuspan@gmail.com>
2014-11-23 20:43:07 -0500

    more work

--- Code/submit_profile_form.php
+++ Code/submit_profile_form.php
@@ -4,7 +4,10 @@
 <title>SPSU Dating site</title>
 <link rel="stylesheet" type="text/css" href="css/style1.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<?php session_start(); ?>
+<?php
+	session_start();
+	error_reporting(E_ALL);
+?>
 </head>
 <body>
 <?php echo 'uid : '.$_SESSION["uid"]; ?>
