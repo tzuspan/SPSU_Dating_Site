6f0043a0dd06a69c45303ef8fc27352100484e36
tzuspan <tzuspan@gmail.com>
2014-11-23 19:48:50 -0500

    more work

--- Code/submit_profile.php
+++ Code/submit_profile.php
@@ -1,7 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>SPSU Dating site</title>
+<link rel="stylesheet" type="text/css" href="css/style1.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php session_start(); ?>
+</head>
+<body>
+
 <?php
 session_start();
 
 include 'dbconnection.php';
+include 'navigation_bar.php';
 
 $tbl_name="tbl_users"; // Table name
 
@@ -32,25 +43,36 @@ else {
 	echo 'SQL: '.$sql.'<br />';
 }
 
-INSERT INTO `swe3613_db05p2`.`tbl_users`
-(`user_id`,
-`email_address`,
-`password`,
-`gender`,
-`age`,
-`first_name`,
-`last_name`,
-`sexual_orientation`,
-`about_me`)
+$query = "INSERT INTO tbl_users
+(
+	email_address,
+	password,
+	gender,
+	age,
+	first_name,
+	last_name,
+	male_friend,
+	female_friend,
+	male_date,
+	female_date,
+	about_me
+)
 VALUES
-(<{user_id: }>,
-<{email_address: }>,
-<{password: }>,
-<{gender: }>,
-<{age: }>,
-<{first_name: }>,
-<{last_name: }>,
-<{sexual_orientation: }>,
-<{about_me: }>);
+(
+	'$user_id',
+	'$email_address',
+	'$password',
+	'$gender',
+	'$age',
+	'$first_name',
+	'$last_name',
+	'$mf,
+	'$ff,
+	'$md,
+	'$fd,
+	'$about_me'
+)";
+
+$result = mysqli_query($dbcon, $query);
 
 ?>
